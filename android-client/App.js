5e910c8a6b328ea8a58fdc2ba670f6baada1b314
adityabpradana <adityabpradana@gmail.com>
2019-09-20 20:18:20 +0700

    skeleton file and folders

--- android-client/App.js
+++ android-client/App.js
@@ -1,19 +1,91 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { createAppContainer, createSwitchNavigator } from 'react-navigation';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from '@apollo/react-hooks';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
+import { createStackNavigator } from 'react-navigation-stack';
 
-export default function App() {
+import LandingScreen from './src/screens/Landing';
+import LoginScreen from './src/screens/Signin';
+import SignupScreen from './src/screens/Signup';
+
+import HomePageScreen from './src/screens/Homepage';
+import HistoryScreen from './src/screens/History';
+import UserScreen from './src/screens/User';
+
+import SelectLocationScreen from './src/screens/SelectLocation';
+import SelectCompanyScreen from './src/screens/SelectCompany';
+import SelectProblemScreen from './src/screens/SelectProblem';
+import ReviewScreen from './src/screens/Review';
+import DoneScreen from './src/screens/Done';
+
+const queueStack = createStackNavigator({
+  HomePage: {
+    screen: HomePageScreen,
+  },
+  SelectLocation: {
+    screen: SelectLocationScreen,
+  },
+  SelectCompany: {
+    screen: SelectCompanyScreen,
+  },
+  SelectProblem: {
+    screen: SelectProblemScreen,
+  },
+  Review: {
+    screen: ReviewScreen
+  },
+  Done: {
+    screen: DoneScreen,
+  }
+},{
+
+});
+
+const homeTab = createBottomTabNavigator({
+  Queue: {
+    screen: queueStack
+  },
+  History: {
+    screen: HistoryScreen,
+  },
+  User: {
+    screen: UserScreen,
+  },
+},{
+
+});
+
+const rootSwitch = createSwitchNavigator({
+  Landing: {
+    screen: LandingScreen
+  },
+  Login: {
+    screen: LoginScreen,
+  },
+  Signup: {
+    screen: SignupScreen,
+  },
+  Home: {
+    screen: homeTab,
+  }
+  
+},{
+  initialRouteName: 'Landing'
+})
+
+const Navigation = createAppContainer(rootSwitch)
+
+const client = new ApolloClient({
+  uri: 'http://localhost:3000/graphql',
+});
+
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+    <ApolloProvider client={client}>
+      <Navigation/>
+    </ApolloProvider>
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
